0e526fbe49bf31b5f158f67ad71569d284841dc4
Abdullajon97 <abdullajonibrohimov0@gmail.com>
2022-11-10 23:57:05 +0500

    5-oy 1-vazifa

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import logo from './image/LOGO.png'
+export default function Header() {
+  return (
+    <>
+      <header className='header__inner'>
+      <div className="container">
+        <nav className="navbar">
+          <a className='navbar__link' href="#"><img src={logo} alt="LOGO" /></a>
+          <ul className="navbar__list">
+            <li className="navbar__list__item"><a className='navbar__list__item__li' href="#Beranda"></a></li>
+            <li className="navbar__list__item"><a className='navbar__list__item__li' href="#">Koleksi</a></li>
+            <li className="navbar__list__item"><a className='navbar__list__item__li' href="#">Syarat dan Ketentuan</a></li>
+            <li className="navbar__list__item"><a className='navbar__list__item__li' href="#">Kontak</a></li>
+            <li className="navbar__list__item"><a className="navbar__list__item__link" href="#">Masuk</a></li>
+          </ul>
+        </nav>
+      </div>
+      </header>
+    </>
+  )
+}
