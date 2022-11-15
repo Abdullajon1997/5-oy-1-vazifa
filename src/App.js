15c824ffcca06c2485b21bccb8398488ac1e7961
Abdullajon97 <abdullajonibrohimov0@gmail.com>
2022-11-15 23:51:39 +0500

    chala

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './../src/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main from './components/Main';
+import Pokoj from "./components/Pokoj/Pokoj"
+import Totto from './components/Totto/Totto';
+import Kita from './components/Kita/Kita';
+import Judul from './components/Judul/Judul';
+import Mereka from './components/Mereka/Mereka';
+import Basa from './components/Basa/Basa';
+import Membantu from './components/Membantu/Membantu';
+
 function App() {
   return (
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
+      <Header />
+      <main>
+        <Pokoj/>
+        <Totto/>
+        <Kita/>
+        <Judul/>
+        <Mereka/>
+        <Basa/>
+        <Membantu/>
+      </main>
+      <Footer />
     </>
-    
-    
+
+
   );
 }
 
